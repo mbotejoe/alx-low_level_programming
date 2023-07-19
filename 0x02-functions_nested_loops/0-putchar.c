4b455edcb6e41da8e1cfb9b92bed94291e3e0474
mbotejoe <mbotejoe@gmail.com>
2023-07-19 16:31:57 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
+ * main -Entry point of my code,
+ *
+ * Return: Always 0
  */
 
 int main(void)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
+#ifndef main_h
+#define main_h
 
 int _putchar(char);
 void print_alphabet(void);
@@ -15,5 +15,5 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-endif
+#endif
 
